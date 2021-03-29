969720fb136be642447eedbafdec7791b1b7524e
BuildTools <admin@ydb.ovh>
2021-03-29 14:18:51 +0200

    Added removeGuildMember

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -5,4 +5,5 @@ export const guild = {
     getGuildMember: discordApi+"/guilds/{guild.id}/members/{user.id}",
     getGuildChannels: discordApi+"/guilds/{guild.id}/channels",
     getGuildMembers: discordApi+"/guilds/{guild.id}/members",
+    removeGuildMember: discordApi+"/guilds/{guild.id}/members/{user.id}"
 }
